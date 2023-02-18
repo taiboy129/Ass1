a272a11a4083b8bb2875deb4be2f4b8125cbf570
taiboy129 <125749280+taiboy129@users.noreply.github.com>
2023-02-18 22:38:40 +0700

    Update API.h

--- API.h
+++ API.h
@@ -1,10 +1,10 @@
-int khoiTaoMang(int arraySize);
-int nhapMang(int array[], int arraySize);
-int hienThiMang(int array[], int arraySize);
-int themPhanTu(int array[], int *arraySize, int number, int position);
-int xoaPhanTu(int array[], int *arraySize, int position);
-int swap(int *value1, int *value2);
-int sapXepGiamDan(int array[], int arraySize);
-int sapXepTangDan(int array[], int arraySize);
-int timKiemMang(int array[], int arraySize, int value);
+int khoiTaoMang(int ktMang);
+int nhapMang(int array[], int ktMang);
+int hienThiMang(int array[], int ktMang);
+int themPhanTu(int array[], int *ktMang, int so, int vitri);
+int xoaPhanTu(int array[], int *ktMang, int vitri);
+int swap(int *giatri1, int *giatri2);
+int sapXepGiamDan(int array[], int ktMang);
+int sapXepTangDan(int array[], int ktMang);
+int timKiemMang(int array[], int ktMange, int giatri);
 
